7a93f3dec70ef2336f6b129d4be8ca6f34cddaf3
Maximilian Friedmann <max@smallstack.io>
2017-09-01 10:36:46 +0200

    more verbose

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,8 @@
 set -e
 if [[ $ARCHIVE_URL ]]; then
   echo "Downloading archive..."
-  cd /tmp
   curl -L -o archive.tar.gz $ARCHIVE_URL
+  echo "Download completed, extracting archive to $HTML_ROOT..."
   tar -xzf archive.tar.gz --directory $HTML_ROOT
 else
   echo "=> Please provide a ARCHIVE_URL environment variable!"
